55c06eb19a18517de9dee5c59a3ce12bbf7f3d69
IsuruKavi <isurukavindalakshan863@gmail.com>
2025-06-10 10:36:46 +0530

    adding fetch data

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -23,10 +23,10 @@ export default async function Page() {
           type="customers"
         /> */}
       </div>
-      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+      {/* <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <RevenueChart revenue={revenue}  />
         <LatestInvoices latestInvoices={latestInvoices} />
-      </div>
+      </div> */}
     </main>
   );
 }
